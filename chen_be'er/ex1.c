971b63138c294eae86dbc2ff89fec63624790a10
Tal0704 <talmeron0704@gmail.com>
2021-09-09 11:06:19 +0300

    fixing brackets

--- chen_be'er/ex1.c
+++ chen_be'er/ex1.c
@@ -3,24 +3,24 @@
 void delay_ms(int ms);
 void Init_Device();
 
-sbit sw1 = P2^0;
-sbit sw2 = P2^1;
+sbit sw1 = P2 ^ 0;
+sbit sw2 = P2 ^ 1;
 
 void main()
 {
-	char hexCodes[] = {0x7E, 0x30, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B};
-	sw = 1;
+    char hexCodes[] = {0x7E, 0x30, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B};
+    sw = 1;
 
-	while(1)
-	{
-		if(!sw1)
+    while (1)
+    {
+        if (!sw1)
             // up
             for (int i = 0; i < 9; i++)
             {
                 P1 = hexCodes[i];
                 delay_ms(500);
             }
-		else
+        else
         {
             if (sw2)
                 // down
@@ -33,5 +33,5 @@ void main()
                 // off
                 P1 = 0x00;
         }
-	}
+    }
 }
